f8d4e8a82ba30c4f223cfb7c6368545226ac0de1
Adam Mwaniki <adamndegwa@protonmail.com>
2025-08-27 19:02:05 +0300

    loading screen text gradient

--- frontend/views/loadingScreen_templ.go
+++ frontend/views/loadingScreen_templ.go
@@ -37,7 +37,7 @@ func LoadingScreen() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<h3 class=\"relative m-0 p-0 font-medium text-center uppercase text-black text-6xl tracking-[14px] overflow-hidden \">Andika</h3></div></section>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</div></section>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -66,7 +66,7 @@ func LogoBlack(black bool) templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<div class=\"andika\"><span></span> <span></span> <span></span></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<div class=\"andika\"><span></span> <span></span> <span></span></div><h3 class=\"relative m-0 p-0 font-medium text-center uppercase text-black text-6xl tracking-[14px] overflow-hidden \">Andika</h3>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
