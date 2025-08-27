23ae1e47b0e2a6922fc85bf7068ed5993f313fab
Adam Mwaniki <adamndegwa@protonmail.com>
2025-08-27 18:44:05 +0300

    temp white loading screen bg

--- frontend/views/loadingScreen_templ.go
+++ frontend/views/loadingScreen_templ.go
@@ -29,7 +29,7 @@ func LoadingScreen() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section id=\"loadingScreen\" class=\"h-screen flex justify-center items-center bg-black\"><div class=\"logo\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section id=\"loadingScreen\" class=\"h-screen flex justify-center items-center bg-white\"><div class=\"logo\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
