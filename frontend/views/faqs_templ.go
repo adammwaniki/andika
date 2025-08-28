63b5e87bc2558d73c983b2157b5c95f4f0696a1f
Adam Mwaniki <adamndegwa@protonmail.com>
2025-08-28 18:38:30 +0300

    feat: adds FAQs

--- frontend/views/faqs_templ.go
+++ frontend/views/faqs_templ.go
@@ -8,7 +8,7 @@ package views
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func FAQs() templ.Component {
+func FAQs(faqs []FAQ) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -29,7 +29,43 @@ func FAQs() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section class=\"sectionSize items-start pt-8 md:pt-36 bg-black text-white\"><div><h2 class=\"secondaryTitle bg-highlight3 p-10 mb-0 bg-center bg-100%\">FAQ</h2></div><div toggleElement class=\"w-full py-4\"><div class=\"flex justify-between items-center\"><div question class=\"font-montserrat font-medium mr-auto\">Where can I get this HTML template?</div><img src=\"dist/assets/logos/CaretRight.svg\" alt=\"\" class=\"transform transition-transform\"></div><div answer class=\"font-montserrat text-sm font-extralight pb-8 hidden\">You can download it on Gumroad.com</div></div><hr class=\"w-full bg-white\"><div toggleElement class=\"w-full py-4\"><div class=\"flex justify-between items-center\"><div question class=\"font-montserrat font-medium mr-auto\">Is this HTML template free?</div><img src=\"dist/assets/logos/CaretRight.svg\" alt=\"\" class=\"transform transition-transform\"></div><div answer class=\"font-montserrat text-sm font-extralight pb-8 hidden\">Yes! For you it is free.</div></div><hr class=\"w-full bg-white\"><div toggleElement class=\"w-full py-4\"><div class=\"flex justify-between items-center\"><div question class=\"font-montserrat font-medium mr-auto\">Am I awesome?</div><img src=\"dist/assets/logos/CaretRight.svg\" alt=\"\" class=\"transform transition-transform\"></div><div answer class=\"font-montserrat text-sm font-extralight pb-8 hidden\">Yes! No doubt about it.</div></div><hr class=\"w-full bg-white\"></section>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section class=\"sectionSize items-start pt-8 md:pt-36 bg-black text-white\"><div><h2 class=\"secondaryTitle bg-highlight3 p-14 mb-0 bg-center bg-100%\">FAQs</h2></div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		for _, faq := range faqs {
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<div toggleElement class=\"w-full py-4\"><div class=\"flex justify-between items-center\"><div question class=\"font-medium mr-auto\">")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var2 string
+			templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(faq.Question)
+			if templ_7745c5c3_Err != nil {
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/faqs.templ`, Line: 15, Col: 26}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</div><img src=\"/images/CaretRight.svg\" alt=\"\" class=\"transform transition-transform\"></div><div answer class=\"text-sm font-extralight pb-8 hidden\">")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var3 string
+			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(faq.Answer)
+			if templ_7745c5c3_Err != nil {
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/faqs.templ`, Line: 20, Col: 22}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "</div></div><hr class=\"w-full bg-white\">")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "</section>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- frontend/views/types.go
+++ frontend/views/types.go
@@ -7,6 +7,11 @@ type Plan struct {
 	OffsetClass   string
 }
 
+type FAQ struct {
+	Question string
+	Answer   string
+}
+
 // Capital P because it needs to be exportable
 var Plans = []Plan{
 	{
@@ -26,6 +31,33 @@ var Plans = []Plan{
 		Price:    		"$TBA",
 		Benefits: 		[]string{"Patco mbili", "Chips mwitu", "Smocha kama tano"},
 		OffsetClass:   	"md:top-24",
+	},
+}
+
+var FAQsData = []FAQ{
+	{
+		Question: "What is Andika?",
+		Answer:   "Andika is a lightweight version control system for personal notes, inspired by Git. It lets you create, edit, snapshot, and restore notes from the terminal or API.",
+	},
+	{
+		Question: "Why not just use Git for notes?",
+		Answer:   "Git is powerful, but it comes with overhead. Andika is purpose-built for quick note-taking with simple commands i.e., no commits, staging, or complex branching required.",
+	},
+	{
+		Question: "How are snapshots stored?",
+		Answer:   "Each note has its own subdirectory within a vcs_storage directory that we automatically create locally, and every snapshot is stored with a unique hash. This keeps history organized and makes restores easy.",
+	},
+	{
+		Question: "Can I restore a previous version of a note?",
+		Answer:   "Yes! You can list snapshots chronologically and restore a note to any previous version.",
+	},
+	{
+		Question: "Is there an API version of this?",
+		Answer:   "Absolutely. The core logic is reusable across CLI and HTTP API, so you can integrate Andika into other apps or services.",
+	},
+	{
+		Question: "What are the real-world use cases?",
+		Answer:   "Journaling, research notes, writing drafts, tracking ideas, or any workflow where you need a reliable “undo button” for text.",
 	},
 }
 
